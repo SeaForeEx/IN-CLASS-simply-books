a98c83719ea2f57af49527f1cd45e9af832879af
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-01-20 13:30:34 -0600

    wrote some notes in comments

--- utils/context/authContext.js
+++ utils/context/authContext.js
@@ -29,7 +29,8 @@ const AuthProvider = (props) => {
         setUser(false);
       }
     }); // creates a single global listener for auth state changed
-  }, []);
+  }, []); // empty dependency array only renders once during initial render
+  // useEffect will only run once even if component's state/props change
 
   const value = useMemo( // https://reactjs.org/docs/hooks-reference.html#usememo
     () => ({
@@ -38,11 +39,20 @@ const AuthProvider = (props) => {
       // as long as user === null, will be true
       // As soon as the user value !== null, value will be false
     }),
-    [user],
+    [user], // useMemo will run every time user state changes
   );
+    // absent dependency arrays can cause infinite loops
+    // function might also re-run during every render, slows the app
+    // dependency arrays ensure that component only updates
+    // when the necessary data changes, improves app performance
 
   return <AuthContext.Provider value={value} {...props} />;
-};
+}; // provides AuthContext context to children components
+// {value} is the prop that came from useMemo to be sent to child components
+
+// AC.Provider shares relevant authentication data to other components that need access
+// separates auth logic from rest of app to make data sharing easier
+
 const AuthConsumer = AuthContext.Consumer;
 
 const useAuth = () => {
